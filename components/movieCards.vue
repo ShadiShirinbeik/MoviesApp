bedb1de4b974ca819466f79beeef031e535c41da
Lenovo <shadi.shirinbeik1994@outlook.com>
2022-09-07 23:32:08 +0430

    movie details UI

--- components/movieCards.vue
+++ components/movieCards.vue
@@ -1,19 +1,21 @@
 <template>
-    <div class="flex movie-cards box-border rounded-md bg-card">
-        <!-- img -->
-        <div class="movie-cards_img">
-            <img src="/images/cover.jpg" alt="movie image">
-        </div>
-        <!-- content -->
-        <div class="movie-cards_content cards-content flex flex-col justify-between">
-            <span class="cards-content_title">Inception</span>
-            <div class="cards-content_footer">
-                <span>2014-20-20</span>
-                <ul class="flex">
-                    <li>Action</li>
-                    <li>Dram</li>
-                </ul>
+    <nuxt-link to="/movie/2022">
+        <div class="flex movie-cards box-border rounded-md bg-card">
+            <!-- img -->
+            <div class="movie-cards_img">
+                <img src="/images/cover.jpg" alt="movie image">
+            </div>
+            <!-- content -->
+            <div class="movie-cards_content cards-content flex flex-col justify-between">
+                <span class="cards-content_title">Inception</span>
+                <div class="cards-content_footer">
+                    <span>2014-20-20</span>
+                    <ul class="flex">
+                        <li>Action</li>
+                        <li>Dram</li>
+                    </ul>
+                </div>
             </div>
         </div>
-    </div>
+    </nuxt-link>
 </template>
---
+++ components/movieDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="details">
+        <!-- movie info -->
+        <div class="flex">
+            <img class="details_cover" src="/images/cover.jpg" alt="movie image">
+            <div class="details_info">
+                <div v-for="i in 6" :key="i">
+                    <span>test:</span>
+                    <span>$1700,000,000</span>
+                </div>
+            </div>
+        </div>
+        <!-- description -->
+        <div class="details_desc">
+            <span>In publishing and graphic design, Lorem ipsum is a In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</span>
+        </div>
+        <!-- credit -->
+        <div class="details_credit credit flex flex-col">
+            <span class="credit_title">Credits:</span>
+            <span class="credit_info">Test, Test, Test, Test</span>
+        </div>
+    </div>
+</template>
